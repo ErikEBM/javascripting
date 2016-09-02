de962d4a116d7f7e28e0706809af5c16590cfd54
ccbenavides <ccbenavidesmontenegro@gmail.com>
2016-09-01 21:27:54 -0500

    nombre

---
+++ lite/main.js
@@ -0,0 +1,107 @@
+// var 
+// crea una variable 
+ 
+var acuario = "asdasd";
+
+// if 
+//  else 
+// typeof
+
+if( typeof [2,3,4,"sadad"] == "object"){
+    // contenido
+} else {
+    // contenido
+}
+
+
+// const 
+
+const nombre = "asdasdasd"; 
+
+// loop / ciclos  
+// for
+// while 
+// do - while
+
+
+// sintaxis de for 
+for(var i = 1 ; i < 5 ; i++){
+}
+
+// sintaxis de while
+var acua = true; 
+while(acua == true){
+    acua = false;
+}
+
+// function
+ 
+
+
+function calcular_dolar(cant_dolar){
+
+//    console.log("aprendiendo javascript");
+    const dolar = 3.4;
+    var total  = dolar * cant_dolar;
+//    console.log("tengo " + total);
+
+}
+
+calcular_dolar(5); 
+calcular_dolar(2);
+calcular_dolar(5);
+calcular_dolar(7);
+
+
+// class 
+class Persona {
+    
+    // es la primera funcion que 
+    // se ejecuta cuando se 
+    // instancia esta clase 
+    // es la unica.
+
+    constructor(nombre, apellidos){
+        // propiedades
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+    }
+
+    // metodo
+    sumar(){
+        console.log("sumar dos numeros");
+    }
+
+}
+
+// creando un objecto a partir de 
+// una clase
+// instanciando
+var christian = new Persona("carlos","benavides");
+console.log(christian.nombre); 
+console.log(Math.round(3.2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ looping-through-arrays.js
@@ -0,0 +1,5 @@
+var pets = ["cat", "dog", "rat"];
+for(var nueva = 0; nueva <= 2; nueva++){
+pets[nueva] = pets[nueva] + "s";
+}
+console.log(pets);
