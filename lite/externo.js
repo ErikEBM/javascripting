e9380d07b2bcd674816a1929b9d3cb340c774ff6
ccbenavides <ccbenavidesmontenegro@gmail.com>
2016-09-02 20:18:45 -0500

    segundo

---
+++ lite/externo.js
@@ -0,0 +1,56 @@
+// Number -> enteros y flotantes
+// String
+// Bolean
+// object -> arrays y objectos
+// null y undefined 
+
+
+
+
+// declaracion de variables
+var entero = 2;
+var flontante = 2.3;
+var cadena = "1";
+var boleano = true;
+var array = ["21", 2, true];
+
+var objecto = {
+    dni : "71433272",
+    nombre : "Carlos Christian",
+    apellido : "Benavides Montenegro",
+    edad : 25,
+    peso : 70,
+    estado : "Soltero",
+    profesion : "Egresado de ingenieria en informatica",
+    gustos : ["arroz con pollo", "arroz con mariscos"],
+    mayor_edad : true,
+    pertenencias : {
+        maquinas : ["latap asus", "mouses"],
+        muebles : "cama"
+    } 
+}
+
+
+var lenguajes =  {
+    cliente : ["css", "html", "javascript"],
+    servidor : ["python", 
+              "php", 
+              "java", 
+              "ruby", 
+              "visual basic",
+              "c ++",
+              "c",
+              "pearl",
+              "grovy"]
+}
+
+
+var color = {
+    rojo:"#f00",
+    verde:"#0f0",
+    azul:"#00f",
+    cyan:"#0ff",
+    magenta:"#f0f",
+    amarillo:"#ff0",
+    negro:"#000"
+}
